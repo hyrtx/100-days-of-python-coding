1b5a3375a87560615232d279132587ae78c95fde
Hyran Teixeira <hyranteixeirastd@gmail.com>
2024-08-07 17:39:03 -0300

    Fixing the quotes in the str variables

--- day04_rock_paper_scissors/rock_paper_scissors.py
+++ day04_rock_paper_scissors/rock_paper_scissors.py
@@ -6,7 +6,7 @@ print(art.logo)
 # List with the game choices where 0 = Rock, 1 = Paper and 2 = Scissor.
 game_choices: list = [0, 1, 2]
 game_images: list = [art.rock_image, art.paper_image, art.scissor_image]
-choice_name: list = ['rock', 'paper', 'scissor']
+choice_name: list = ["rock", "paper", "scissor"]
 
 # Computer choice
 computer_choice: int = choice(game_choices)
@@ -23,44 +23,38 @@ while not valid_choice:
         print("Please, select a valid option")
 
 # Match
-print(f'You choose {choice_name[player_choice].capitalize()}\n')
+print(f"You choose {choice_name[player_choice].capitalize()}\n")
 print(game_images[player_choice])
 
-print(f'\n\nComputer choose {choice_name[computer_choice].capitalize()}\n')
-print(f'{game_images[computer_choice]}\n')
+print(f"\n\nComputer choose {choice_name[computer_choice].capitalize()}\n")
+print(f"{game_images[computer_choice]}\n")
 
 # Result if player chooses rock
 if player_choice == 0:
 
   if computer_choice == 0:
-    print('Draw')
+    print("Draw")
   elif computer_choice == 1:
-    print('You lose. Try Again!')
+    print("You lose. Try Again!")
   elif computer_choice == 2:
-    print('Congratulations! You won')
+    print("Congratulations! You won")
 
 # Result if player chooses paper
 elif player_choice == 1:
 
   if computer_choice == 0:
-    print('Congratulations! You won')
+    print("Congratulations! You won")
   elif computer_choice == 1:
-    print('Draw')
+    print("Draw")
   elif computer_choice == 2:
-    print('You lose. Try Again!')
+    print("You lose. Try Again!")
 
 # Result if player chooses scissors
 elif player_choice == 2:
 
   if computer_choice == 0:
-    print('You lose. Try Again!')
+    print("You lose. Try Again!")
   elif computer_choice == 1:
-    print('Congratulations! You won')
+    print("Congratulations! You won")
   elif computer_choice == 2:
-    print('Draw')
-
-
-
-                            
-
-
+    print("Draw")
