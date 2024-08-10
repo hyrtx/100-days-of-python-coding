f418a6c516ee4981aebfb65c0d0939e2b33e7bb5
Hyran Teixeira <hyranteixeirastd@gmail.com>
2024-08-10 20:08:59 -0300

    Updating the code

--- day10_blackjack/main.py
+++ day10_blackjack/main.py
@@ -19,12 +19,10 @@ def get_card():
     
     return choice(list(cards.values()))
 
-player_hand: list = []
-
 def get_dealer_hand():
     """Draw the dealer cards from the deck"""
     dealer_hand: list = []
-    card = get_card()
+    card: int = get_card()
 
     while sum(dealer_hand) < 18:
 
@@ -35,4 +33,49 @@ def get_dealer_hand():
     
     return dealer_hand
 
-print(get_dealer_hand())
+dealer_hand: list = get_dealer_hand()
+player_hand: list = []
+
+for i in range(0, 2):
+    card: int = get_card()
+
+    if card == 11 and sum(player_hand) > 10:
+        player_hand.append(1)
+    else:
+        player_hand.append(card)
+
+while sum(player_hand) < 21:
+    print(f"    Your cards: {player_hand}, current score: {sum(player_hand)}")
+    print(f"    Dealer's first card: {dealer_hand[0]}")
+    
+    draw_flag: str = input("Type 'y' to get another card, type 'n' to pass: ").strip().lower()
+    
+    if draw_flag == "y":
+
+        if card == 11 and sum(player_hand) > 10:
+            player_hand.append(1)
+        else:
+            player_hand.append(card)
+    
+    else:
+        break
+
+if sum(player_hand) > 21:
+    print(f"   Your final hand: {player_hand}, total score: {sum(player_hand)}")
+    print(f"   Dealer's final hand: [{dealer_hand[0]}], total score: {dealer_hand[0]}")
+    print("You busted! Dealer Wins")
+
+elif sum(dealer_hand) > 21:
+    print(f"   Your final hand: {player_hand}, total score: {sum(player_hand)}")
+    print(f"   Dealer's final hand: {dealer_hand}, total score: {sum(dealer_hand)}")
+    print("You Won! Congratulations!")
+
+elif sum(player_hand) > sum(dealer_hand):
+    print(f"   Your final hand: {player_hand}, total score: {sum(player_hand)}")
+    print(f"   Dealer's final hand: {dealer_hand}, total score: {sum(dealer_hand)}")
+    print("You Won! Congratulations!")
+
+else:
+    print(f"   Your final hand: {player_hand}, total score: {sum(player_hand)}")
+    print(f"   Dealer's final hand: {dealer_hand}, total score: {sum(dealer_hand)}")
+    print("Dealer wins! Congratulations!")
