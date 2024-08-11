bb977b7b9fd163ca230511570823fb1b67bf1fa8
Hyran Teixeira <hyranteixeirastd@gmail.com>
2024-08-10 21:08:57 -0300

    Improving the code with more functions

--- day10_blackjack/main.py
+++ day10_blackjack/main.py
@@ -1,8 +1,6 @@
 from art import logo
 from random import choice
 
-print(logo)
-
 def get_card():
     """Function to get a card from the deck"""
     cards: dict = {
@@ -25,25 +23,39 @@ def get_dealer_hand():
     card: int = get_card()
 
     while sum(dealer_hand) < 18:
-
         if card == 11 and sum(dealer_hand) > 10:
             dealer_hand.append(1)
         else:    
             dealer_hand.append(card)
-    
     return dealer_hand
+
+def get_player_card():
+    """Draw one card for the player"""
+    card: int = get_card()
+
+    if card == 11 and sum(player_hand) > 10:
+        return 1
+    else:
+        return card
+
+def result(player_hand: list, dealer_hand: list) -> str:
+    """Determine the game result"""
+    if sum(player_hand) > 21:
+        return "You busted! Dealer Wins"
+    elif sum(dealer_hand) > 21 or sum(player_hand) > sum(dealer_hand):
+        return "You Won! Congratulations!"
+    else:
+        return "Dealer wins! Congratulations!"
+
 
 dealer_hand: list = get_dealer_hand()
 player_hand: list = []
 
+# Drawing the first two player's cards
 for i in range(0, 2):
-    card: int = get_card()
+    player_hand.append(get_card)
 
-    if card == 11 and sum(player_hand) > 10:
-        player_hand.append(1)
-    else:
-        player_hand.append(card)
-
+# Giving the player the choice to draw another card
 while sum(player_hand) < 21:
     print(f"    Your cards: {player_hand}, current score: {sum(player_hand)}")
     print(f"    Dealer's first card: {dealer_hand[0]}")
@@ -51,31 +63,14 @@ while sum(player_hand) < 21:
     draw_flag: str = input("Type 'y' to get another card, type 'n' to pass: ").strip().lower()
     
     if draw_flag == "y":
-
+        card = get_player_card()
         if card == 11 and sum(player_hand) > 10:
             player_hand.append(1)
         else:
             player_hand.append(card)
-    
     else:
         break
 
-if sum(player_hand) > 21:
-    print(f"   Your final hand: {player_hand}, total score: {sum(player_hand)}")
-    print(f"   Dealer's final hand: [{dealer_hand[0]}], total score: {dealer_hand[0]}")
-    print("You busted! Dealer Wins")
-
-elif sum(dealer_hand) > 21:
-    print(f"   Your final hand: {player_hand}, total score: {sum(player_hand)}")
-    print(f"   Dealer's final hand: {dealer_hand}, total score: {sum(dealer_hand)}")
-    print("You Won! Congratulations!")
-
-elif sum(player_hand) > sum(dealer_hand):
-    print(f"   Your final hand: {player_hand}, total score: {sum(player_hand)}")
-    print(f"   Dealer's final hand: {dealer_hand}, total score: {sum(dealer_hand)}")
-    print("You Won! Congratulations!")
-
-else:
-    print(f"   Your final hand: {player_hand}, total score: {sum(player_hand)}")
-    print(f"   Dealer's final hand: {dealer_hand}, total score: {sum(dealer_hand)}")
-    print("Dealer wins! Congratulations!")
+print(f"   Your final hand: {player_hand}, total score: {sum(player_hand)}")
+print(f"   Dealer's final hand: {dealer_hand}, total score: {sum(dealer_hand)}")
+print(result)
