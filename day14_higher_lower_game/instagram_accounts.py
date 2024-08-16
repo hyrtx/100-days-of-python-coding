517b30519abe38d070783c0c804c48e8a2e16023
Hyran Teixeira <hyranteixeirastd@gmail.com>
2024-08-16 14:07:11 -0300

    Creating the file with the instagram accounts dict

---
+++ day14_higher_lower_game/instagram_accounts.py
@@ -0,0 +1,302 @@
+instagram_accounts = {
+    "@instagram": {
+        "name": "Instagram",
+        "description": "Social media platform",
+        "country": "United States",
+        "followers_millions": 675
+    },
+    "@cristiano": {
+        "name": "Cristiano Ronaldo",
+        "description": "Footballer",
+        "country": "Portugal",
+        "followers_millions": 635
+    },
+    "@leomessi": {
+        "name": "Lionel Messi",
+        "description": "Footballer",
+        "country": "Argentina",
+        "followers_millions": 504
+    },
+    "@selenagomez": {
+        "name": "Selena Gomez",
+        "description": "Musician and actress",
+        "country": "United States",
+        "followers_millions": 425
+    },
+    "@kyliejenner": {
+        "name": "Kylie Jenner",
+        "description": "Media personality",
+        "country": "United States",
+        "followers_millions": 398
+    },
+    "@therock": {
+        "name": "Dwayne Johnson",
+        "description": "Actor and professional wrestler",
+        "country": "United States",
+        "followers_millions": 396
+    },
+    "@arianagrande": {
+        "name": "Ariana Grande",
+        "description": "Musician and actress",
+        "country": "United States",
+        "followers_millions": 377
+    },
+    "@kimkardashian": {
+        "name": "Kim Kardashian",
+        "description": "Media personality",
+        "country": "United States",
+        "followers_millions": 361
+    },
+    "@beyonce": {
+        "name": "Beyoncé",
+        "description": "Musician and actress",
+        "country": "United States",
+        "followers_millions": 317
+    },
+    "@khloekardashian": {
+        "name": "Khloé Kardashian",
+        "description": "Media personality",
+        "country": "United States",
+        "followers_millions": 307
+    },
+    "@nike": {
+        "name": "Nike",
+        "description": "Sportswear multinational",
+        "country": "United States",
+        "followers_millions": 304
+    },
+    "@justinbieber": {
+        "name": "Justin Bieber",
+        "description": "Musician",
+        "country": "Canada",
+        "followers_millions": 293
+    },
+    "@kendalljenner": {
+        "name": "Kendall Jenner",
+        "description": "Media personality",
+        "country": "United States",
+        "followers_millions": 292
+    },
+    "@taylorswift": {
+        "name": "Taylor Swift",
+        "description": "Musician",
+        "country": "United States",
+        "followers_millions": 283
+    },
+    "@natgeo": {
+        "name": "National Geographic",
+        "description": "Magazine",
+        "country": "United States",
+        "followers_millions": 282
+    },
+    "@virat.kohli": {
+        "name": "Virat Kohli",
+        "description": "Cricketer",
+        "country": "India",
+        "followers_millions": 270
+    },
+    "@jlo": {
+        "name": "Jennifer Lopez",
+        "description": "Musician and actress",
+        "country": "United States",
+        "followers_millions": 251
+    },
+    "@nickiminaj": {
+        "name": "Nicki Minaj",
+        "description": "Musician",
+        "country": "United States, Trinidad and Tobago",
+        "followers_millions": 228
+    },
+    "@neymarjr": {
+        "name": "Neymar",
+        "description": "Footballer",
+        "country": "Brazil",
+        "followers_millions": 223
+    },
+    "@kourtneykardash": {
+        "name": "Kourtney Kardashian",
+        "description": "Media personality",
+        "country": "United States",
+        "followers_millions": 222
+    },
+    "@mileycyrus": {
+        "name": "Miley Cyrus",
+        "description": "Musician and actress",
+        "country": "United States",
+        "followers_millions": 215
+    },
+    "@katyperry": {
+        "name": "Katy Perry",
+        "description": "Musician",
+        "country": "United States",
+        "followers_millions": 206
+    },
+    "@zendaya": {
+        "name": "Zendaya",
+        "description": "Actress and singer",
+        "country": "United States",
+        "followers_millions": 183
+    },
+    "@kevinhart4real": {
+        "name": "Kevin Hart",
+        "description": "Comedian and actor",
+        "country": "United States",
+        "followers_millions": 179
+    },
+    "@iamcardib": {
+        "name": "Cardi B",
+        "description": "Musician and actress",
+        "country": "United States",
+        "followers_millions": 166
+    },
+    "@realmadrid": {
+        "name": "Real Madrid CF",
+        "description": "Football club",
+        "country": "Spain",
+        "followers_millions": 165
+    },
+    "@kingjames": {
+        "name": "LeBron James",
+        "description": "Basketball player",
+        "country": "United States",
+        "followers_millions": 159
+    },
+    "@ddlovato": {
+        "name": "Demi Lovato",
+        "description": "Musician and actress",
+        "country": "United States",
+        "followers_millions": 155
+    },
+    "@badgalriri": {
+        "name": "Rihanna",
+        "description": "Musician",
+        "country": "Barbados",
+        "followers_millions": 151
+    },
+    "@champagnepapi": {
+        "name": "Drake",
+        "description": "Musician",
+        "country": "Canada, United States",
+        "followers_millions": 145
+    },
+    "@chrisbrownofficial": {
+        "name": "Chris Brown",
+        "description": "Musician",
+        "country": "United States",
+        "followers_millions": 145
+    },
+    "@ellendegeneres": {
+        "name": "Ellen DeGeneres",
+        "description": "Comedian and television host",
+        "country": "United States",
+        "followers_millions": 138
+    },
+    "@fcbarcelona": {
+        "name": "FC Barcelona",
+        "description": "Football club",
+        "country": "Spain",
+        "followers_millions": 128
+    },
+    "@k.mbappe": {
+        "name": "Kylian Mbappé",
+        "description": "Footballer",
+        "country": "France",
+        "followers_millions": 121
+    },
+    "@billieeilish": {
+        "name": "Billie Eilish",
+        "description": "Musician",
+        "country": "United States",
+        "followers_millions": 119
+    },
+    "@championsleague": {
+        "name": "UEFA Champions League",
+        "description": "Club football competition",
+        "country": "Europe",
+        "followers_millions": 115
+    },
+    "@gal_gadot": {
+        "name": "Gal Gadot",
+        "description": "Actress",
+        "country": "Israel",
+        "followers_millions": 108
+    },
+    "@lalalalisa_m": {
+        "name": "Lisa",
+        "description": "Musician",
+        "country": "Thailand",
+        "followers_millions": 104
+    },
+    "@vindiesel": {
+        "name": "Vin Diesel",
+        "description": "Actor",
+        "country": "United States",
+        "followers_millions": 102
+    },
+    "@nasa": {
+        "name": "NASA",
+        "description": "Space agency",
+        "country": "United States",
+        "followers_millions": 97.5
+    },
+    "@priyankachopra": {
+        "name": "Priyanka Chopra",
+        "description": "Actress",
+        "country": "India",
+        "followers_millions": 91.7
+    },
+    "@narendramodi": {
+        "name": "Narendra Modi",
+        "description": "Political figure, Prime Minister of India",
+        "country": "India",
+        "followers_millions": 91.3
+    },
+    "@shraddhakapoor": {
+        "name": "Shraddha Kapoor",
+        "description": "Actress",
+        "country": "India",
+        "followers_millions": 91
+    },
+    "@shakira": {
+        "name": "Shakira",
+        "description": "Musician",
+        "country": "Colombia",
+        "followers_millions": 90.5
+    },
+    "@snoopdogg": {
+        "name": "Snoop Dogg",
+        "description": "Musician",
+        "country": "United States",
+        "followers_millions": 88.7
+    },
+    "@nba": {
+        "name": "NBA",
+        "description": "Professional basketball league",
+        "country": "United States, Canada",
+        "followers_millions": 88.6
+    },
+    "@davidbeckham": {
+        "name": "David Beckham",
+        "description": "Former footballer",
+        "country": "United Kingdom",
+        "followers_millions": 88.3
+    },
+    "@dualipa": {
+        "name": "Dua Lipa",
+        "description": "Musician",
+        "country": "United Kingdom, Albania",
+        "followers_millions": 87.9
+    },
+    "@jennierubyjane": {
+        "name": "Jennie",
+        "description": "Musician",
+        "country": "South Korea",
+        "followers_millions": 85.2
+    },
+    "@aliaabhatt": {
+        "name": "Alia Bhatt",
+        "description": "Actress",
+        "country": "United Kingdom, India",
+        "followers_millions": 85.1
+    }
+}
