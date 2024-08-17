ff1977482f76e95ef00bb52a011fdccf72d7c9cb
Hyran Teixeira <hyranteixeirastd@gmail.com>
2024-08-17 09:30:43 -0300

    Creating the function to check resources

--- day15_coffee_machine/main.py
+++ day15_coffee_machine/main.py
@@ -13,30 +13,37 @@ def get_report() -> None:
     unit: str = ""
 
     for k, v in resources.items():
-
         if k == "coffee":
             unit = "g"
         else:
             unit = "ml"
-        
         print(f"{k}: {v}{unit}")
 
     print(f"Money: ${money}")
 
-def check_resources(product: str):
-    return None
+def check_resources(product_choice: str):
+    ingredients: dict = machine.MENU[product_choice]['ingredients']
+
+    for item in ingredients:
+        if ingredients[item] > resources[item]:
+            print(f"Sorry, there aren't enough {item} to prepare a {coffee_choice}")
+            return False
+        else:
+            return True
 
 while True:
     # Prompt user by asking “What would you like? (espresso/latte/cappuccino)
-    choice: str = input("What would you like? (espresso/latte/cappuccino): ")
+    coffee_choice: str = input("What would you like? (espresso/latte/cappuccino): ")
 
     # Turning off option
-    if choice == "off":
+    if coffee_choice == "off":
         print("Turning off the machine...")
 
     # Print report
-    elif choice == "report":
+    elif coffee_choice == "report":
         get_report()
         break
         
 # Check resources sufficient
+    elif coffee_choice in machine.MENU:
+        check_resources(coffee_choice)
