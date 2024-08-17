fe265187a2a4f4a59fe6bf99c10d4a6c7e00194e
Hyran Teixeira <hyranteixeirastd@gmail.com>
2024-08-17 17:03:11 -0300

    Finishing the code and created the system's logo

--- day15_coffee_machine/main.py
+++ day15_coffee_machine/main.py
@@ -1,5 +1,6 @@
-import art
-import machine
+from art import logo
+from time import sleep
+from machine import MENU
 
 resources: dict = {
     'water': 300,
@@ -8,6 +9,11 @@ resources: dict = {
 }
 
 profit: float = 0
+
+def get_product_cost(product_name: str) -> float:
+    '''Get the cost price of a product'''
+    product_cost: float = MENU[product_name]['cost']
+    return product_cost
 
 def get_report() -> None:
     '''Function to print the machine report'''
@@ -24,7 +30,7 @@ def get_report() -> None:
 
 def check_resources(product_choice: str) -> bool:
     '''Check if the machine has the resources to process the coffee'''
-    ingredients: dict = machine.MENU[product_choice]['ingredients']
+    ingredients: dict = MENU[product_choice]['ingredients']
 
     for item in ingredients:
         if ingredients[item] > resources[item]:
@@ -33,7 +39,8 @@ def check_resources(product_choice: str) -> bool:
         else:
             return True
         
-def process_coins():
+def process_coins() -> float:
+    '''Process the payment in coins made by the client'''
     coins: dict = {}
     payment: float = 0
 
@@ -53,30 +60,72 @@ def process_coins():
             print(e)
             print(type(e))
         
-    payment = sum(coins.values)
+    payment = sum(coins.values())
 
     return payment
 
+def check_payment(payment: float, product_choice: str) -> bool:
+    '''Check if the payment made by the client is correct'''
+    product_price: float = get_product_cost(product_choice)
+    change: float = 0.0
+
+    if payment == product_price:
+        return True
+    elif payment > product_price:
+        change = payment - product_price
+        print(f"Here is ${round(change, 2)} dollars in change.")
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+def make_coffee(product_choice: str) -> None:
+    '''Make the coffee and deduct the ingredients used from resources. 
+    There is no return the function'''
+    ingredients: dict = MENU[product_choice]['ingredients']
+
+    for item in ingredients:
+        resources[item] -= ingredients[item]
+    
+    print(f"Here's your {product_choice}. Enjoy!")
+
 while True:
     # Prompt user by asking “What would you like? (espresso/latte/cappuccino)
-    coffee_choice: str = input("What would you like? (espresso/latte/cappuccino): ")
+    print(logo)
+    coffee_choice: str = input("What would you like? (espresso/latte/cappuccino): ").strip().lower()
 
-    # Turning off option
+    # Turning off option for maintenance
     if coffee_choice == 'off':
         print("Turning off the machine...")
+        break
 
     # Print report
     elif coffee_choice == 'report':
         get_report()
-        break
+        sleep(5)
+        continue
         
-    # Check resources sufficient
-    elif coffee_choice in machine.MENU:
+    # Check if resources are sufficient
+    elif coffee_choice in MENU:
         
+        # Convert the coins to dollars
         if check_resources(coffee_choice):
             coffee_payment = process_coins()
-            print(coffee_payment)
+            
+            # Check if the transacion was successful
+            if check_payment(coffee_payment, coffee_choice):
+                profit += get_product_cost(coffee_choice)
+                make_coffee(coffee_choice)
+                sleep(3)
+                continue
+            else:
+                sleep(3)
+                continue
         
         else:
-            print("We don't have this option. Try one of our coffees.")
+            sleep(3)
             continue
+        
+    else:
+        print("We don't have this option. Try one of our coffees.")
+        sleep(3)
