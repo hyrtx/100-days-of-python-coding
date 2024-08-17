bac5a6d52211156959c30175a75f0d1be6cb8193
Hyran Teixeira <hyranteixeirastd@gmail.com>
2024-08-17 15:17:07 -0300

    Creating the function to process code

--- day15_coffee_machine/machine.py
+++ day15_coffee_machine/machine.py
@@ -1,25 +1,32 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18
+MENU: dict = {
+    'espresso': {
+        'ingredients': {
+            'water': 50,
+            'coffee': 18
         },
-        "cost": 1.5
+        'cost': 1.5
     },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "coffee": 24,
-            "milk": 150
+    'latte': {
+        'ingredients': {
+            'water': 200,
+            'coffee': 24,
+            'milk': 150
         },
-        "cost": 2.5
+        'cost': 2.5
     },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "coffee": 24,
-            "milk": 100
+    'cappuccino': {
+        'ingredients': {
+            'water': 250,
+            'coffee': 24,
+            'milk': 100
         },
-        "cost": 3.0
+        'cost': 3.0
     }
+}
+
+COINS: dict = {
+    'quarters': 0.25,
+    'dimes': 0.10,
+    'nickles': 0.5,
+    'pennies': 0.1
 }
--- day15_coffee_machine/main.py
+++ day15_coffee_machine/main.py
@@ -2,26 +2,28 @@ import art
 import machine
 
 resources: dict = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100
+    'water': 300,
+    'milk': 200,
+    'coffee': 100
 }
 
-money: float = 0
+profit: float = 0
 
 def get_report() -> None:
+    '''Function to print the machine report'''
     unit: str = ""
 
     for k, v in resources.items():
-        if k == "coffee":
-            unit = "g"
+        if k == 'coffee':
+            unit = 'g'
         else:
-            unit = "ml"
+            unit = 'ml'
         print(f"{k}: {v}{unit}")
 
-    print(f"Money: ${money}")
+    print(f"Profit: ${profit}")
 
-def check_resources(product_choice: str):
+def check_resources(product_choice: str) -> bool:
+    '''Check if the machine has the resources to process the coffee'''
     ingredients: dict = machine.MENU[product_choice]['ingredients']
 
     for item in ingredients:
@@ -30,20 +32,51 @@ def check_resources(product_choice: str):
             return False
         else:
             return True
+        
+def process_coins():
+    coins: dict = {}
+    payment: float = 0
+
+    print("Please, insert coins")
+
+    while True:
+        try:
+            coins['quarter'] = int(input("How many quarters? ")) * 0.25 # Value of quarter
+            coins['dime'] = int(input("How many dimes? ")) * 0.10 # Value of dime
+            coins['nickel'] = int(input("How many nickes? ")) * 0.05 # Value of nickel
+            coins['penny'] = int(input("How many pennies? ")) * 0.01 # Value of penny
+            break
+        except ValueError:
+            print("Please, insert coins on the machine")
+            continue
+        except Exception as e:
+            print(e)
+            print(type(e))
+        
+    payment = sum(coins.values)
+
+    return payment
 
 while True:
     # Prompt user by asking “What would you like? (espresso/latte/cappuccino)
     coffee_choice: str = input("What would you like? (espresso/latte/cappuccino): ")
 
     # Turning off option
-    if coffee_choice == "off":
+    if coffee_choice == 'off':
         print("Turning off the machine...")
 
     # Print report
-    elif coffee_choice == "report":
+    elif coffee_choice == 'report':
         get_report()
         break
         
-# Check resources sufficient
+    # Check resources sufficient
     elif coffee_choice in machine.MENU:
-        check_resources(coffee_choice)
+        
+        if check_resources(coffee_choice):
+            coffee_payment = process_coins()
+            print(coffee_payment)
+        
+        else:
+            print("We don't have this option. Try one of our coffees.")
+            continue
